5814810445615c77ca0cd9b5b4fc6f69e9cd663c
João Bosco Dos Reis Becker <joao.jbosco@gmail.com>
2024-01-23 17:22:25 -0300

    Adicionando arquivos
    Adicionando arquivos

---
+++ Blazor/Entities/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
+
+namespace BlazorClientesShared.Entities
+{
+    public class Cliente { 
+        public int ID { get; set; }
+        [Required]
+        public string Nome { get; set; } = string.Empty;
+        [Required]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public int Idade { get; set; }
+    }
+}
---
+++ Blazor/Interfaces/IClienteRepository.cs
@@ -0,0 +1,14 @@
+﻿using BlazorClientesShared.Entities;
+
+namespace BlazorClientesShared.Interfaces
+{
+    public interface IClienteRepository
+    {
+        Task<Cliente> AddClienteAsyc(Cliente Model);
+        Task<Cliente> UpdateClienteAsync(Cliente Model);
+        Task<Cliente> DeleteClienteAsync(int clienteId);
+        Task<List<Cliente>> GetAllCLientesAsync();
+        Task<Cliente> GetCLienteByID(int clienteId);
+
+    }
+}
